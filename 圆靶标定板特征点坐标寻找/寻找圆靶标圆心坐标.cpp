4830998b6b80a696056b1d3bf776cf08a9c4c7d1
wy1996-hub <1728587757@qq.com>
2021-07-02 16:34:04 +0800

    提交了一份鲁棒的寻找圆靶标定板圆心坐标的代码

---
+++ 圆靶标定板特征点坐标寻找/寻找圆靶标圆心坐标.cpp
@@ -0,0 +1,104 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <fstream>
+
+using namespace cv;
+using namespace std;
+
+// 边缘检测方法
+int findFP1(const char* filename)
+{
+	Mat img = imread(filename);
+	if (img.empty())
+	{
+		cout << "can not open " << filename << endl;
+		return -1;
+	}
+
+	Mat img3, img2, img4;
+
+	cvtColor(img, img2, COLOR_BGR2GRAY);   //把彩色图转换为黑白图像
+	GaussianBlur(img2, img2, Size(9, 9), 2, 2);
+	threshold(img2, img3, 70, 255, THRESH_BINARY);  //图像二值化，，注意阈值变化
+	namedWindow("detecte circles", WINDOW_NORMAL);
+	imshow("detecte circles", img3);
+	Canny(img3, img3, 60, 100);//边缘检测
+	namedWindow("detect circles", WINDOW_NORMAL);
+	imshow("detect circles", img3);
+	vector<vector<Point>>contours;
+	vector<Vec4i>hierarchy;
+	findContours(img3, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);//查找出所有的圆边界
+	int index = 0;
+	for (; index >= 0; index = hierarchy[index][0])
+	{
+		Scalar color(rand() & 255, rand() & 255, rand() & 255);
+		drawContours(img, contours, index, color, FILLED, 8, hierarchy);
+	}
+
+	namedWindow("detected circles", WINDOW_NORMAL);
+	imshow("detected circles", img);
+	//标准圆在图片上一般是椭圆，所以采用OpenCV中拟合椭圆的方法求解中心
+	Mat pointsf;
+	Mat(contours[0]).convertTo(pointsf, CV_32F);
+	RotatedRect box = fitEllipse(pointsf);
+	cout << box.center;
+	waitKey();
+
+	return 0;
+}
+
+// 动态阈值分割方法
+bool findFP2(Mat srcImage, Rect rect, int kSize, int filterMode = 0, int minArea = 100, int maxArea = 10000)
+{
+	Mat grayImage;
+	cvtColor(srcImage, grayImage, COLOR_BGR2GRAY);
+	Mat viewGray = grayImage(rect);
+	Mat view = srcImage(rect);
+
+	Mat blurImg;
+	switch (filterMode)
+	{
+	case 0:
+	{
+		blur(viewGray, blurImg, Size(kSize, kSize));//性价比高，建议首先尝试
+		break;
+	}
+	case 1:
+	{
+		GaussianBlur(viewGray, blurImg, Size(kSize, kSize), 0);//结果较暗，可能只提取了边缘
+		break;
+	}
+	case 2:
+	{
+		medianBlur(viewGray, blurImg, kSize);//调参数后，效果会最好；有时出问题可以尝试均值滤波
+		break;
+	}
+	default:
+		break;
+	}
+		
+	Mat dst;
+	subtract(viewGray, blurImg, dst);
+	Mat fore = dst.clone();
+	threshold(fore, fore, 0, 255, THRESH_OTSU);
+	bitwise_not(fore, fore);
+
+	Size board_size = Size(9, 11);
+	vector<Point2f> image_points;
+	SimpleBlobDetector::Params parameters = SimpleBlobDetector::Params();
+	parameters.filterByArea = true;  parameters.minArea = minArea; parameters.maxArea = maxArea;
+
+	bool ok = findCirclesGrid(fore, board_size, image_points, CALIB_CB_SYMMETRIC_GRID | CALIB_CB_CLUSTERING, SimpleBlobDetector::create(parameters));
+	if (0 == ok)
+	{
+		cout << "照片提取圆心失败" << endl;
+		return false;
+	}
+	else
+	{
+		cornerSubPix(fore, image_points, Size(1, 1), Size(-1, -1), TermCriteria(TermCriteria::COUNT | TermCriteria::EPS, 30, 0.001));
+		drawChessboardCorners(view, board_size, image_points, true);
+	}
+
+	return true;
+}
